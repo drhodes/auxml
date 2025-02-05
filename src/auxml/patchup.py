a3349918fbb8e348fa42b9fcbabe09aacaf68678
Derek Rhodes <physci@gmail.com>
2025-02-05 13:16:57 -0500

    improves error message for patchups

--- src/auxml/patchup.py
+++ src/auxml/patchup.py
@@ -10,20 +10,28 @@ class PatchupModule():
     def run(self, tree, el):
         func_name = el.get("func")
         if func_name is None:
-            raise Exception("patchup element must have a `func` attribute that specifies patch function: todo import this err")
+            msg = f'''
+            patchup element at {el_location_info(el)}
+            
+            patchup element must have a `func` attribute that
+            specifies patch function, something like:
 
-        func_name = el.get("func")
-        if func_name is None:
-            raise Exception("patchup element must have a `func` attribute that specifies patch function: todo import this err")
+            <patchup func="my_function"/>
+            
+            for more information see docs 
+            '''
+            raise Exception(msg)
 
         if hasattr(self.mod, func_name):
             f = getattr(self.mod, func_name)
-            print(f"patching element with function: {func_name}" + el_location_info(el))
+            # log this instead of stdout.
+            print(f"processing patchup element with function: {func_name}" + el_location_info(el))
             return f(tree, el)
         else:
-            msg = '''
+            msg = f'''
+            patchup element at {el_location_info(el)}
             patchup function with name : `{func_name}` not found in
-            patchup module located at  : `{self.module_path}'
+            patchup module located at  : `{self.mod_path}'
 
             Please check your paths and that the function `{func_name}` actually exists.
             '''
